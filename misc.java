cbd3d810b87e287582f524610dd6d315bc0e0ea6
yoshi8777 <123114539+yoshi8777@users.noreply.github.com>
2023-08-05 08:57:43 +0530

    update

--- misc.java
+++ misc.java
@@ -1,3 +1,23 @@
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getId() + " Value " + i);
+        }
+    }
+}
+
+public class MultiThreadExample {
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread();
+        MyThread thread2 = new MyThread();
+
+        thread1.start();
+        thread2.start();
+    }
+}
+
+
 Pattern Questions:
 
 Question 1:
