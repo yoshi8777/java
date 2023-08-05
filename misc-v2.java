d5efc4368485796412693d10424638fc77db48f0
yoshi8777 <123114539+yoshi8777@users.noreply.github.com>
2023-08-05 08:49:01 +0530

    Update misc-v2.java

--- misc-v2.java
+++ misc-v2.java
@@ -1,3 +1,37 @@
+package example;
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:");
+        int size = scanner.nextInt();
+
+        int[] numbers = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        int sum = calculateSum(numbers);
+        System.out.println("Sum of numbers: " + sum);
+    }
+
+    public static int calculateSum(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
+    }
+}
+
+
 import java.util.Scanner;
 
 public class PatternAndLogicExamples {
